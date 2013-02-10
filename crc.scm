835d28cae809db434b66526d57167a217dc4ce27
Jeff Read <bitwize@gmail.com>
2013-02-10 23:38:51 +0000

    Initial commit

---
+++ crc.scm
@@ -0,0 +1,26 @@
+(define *crc-table* (make-u32vector 256 0))
+
+(define (fill-crc-table! poly)
+  (do ((i 0 (+ i 1)))
+      ((>= i 256))
+    (u32vector-set!
+     *crc-table*
+     i
+     (do ((bit 0 (+ bit 1))
+          (v i
+             (if (zero? (bitwise-and v 1))
+                 (arithmetic-shift v -1)
+                 (bitwise-xor poly (arithmetic-shift v -1)))))
+         ((>= bit 8) v)))))
+
+(define (crc data)
+  (let ((len (u8vector-length data)))
+    (do ((i 0 (+ i 1))
+         (c 4294967295
+            (bitwise-xor
+             (u32vector-ref
+              *crc-table*
+              (bitwise-and (bitwise-xor c (u8vector-ref data i)) 255))
+             (arithmetic-shift c -8))))
+        ((>= i len) (bitwise-xor c 4294967295)))))
+
